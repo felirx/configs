a8480a1b891bc6123a8fdf3867f316ea25a6250b
felirx <felirx@gmail.com>
2024-11-15 15:23:31 +0200

    fix the brew check to actually work

--- fish/conf.d/00_brew.fish
+++ fish/conf.d/00_brew.fish
@@ -1,4 +1,4 @@
-if type -q brew
+if type -q /opt/homebrew/bin/brew
     eval $(/opt/homebrew/bin/brew shellenv)
     fish_add_path "/opt/homebrew/opt/openjdk@21/bin"
     export CPPFLAGS="-I/opt/homebrew/opt/openjdk@11/include"
