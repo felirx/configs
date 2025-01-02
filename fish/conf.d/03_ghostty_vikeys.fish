4f38c624e67b44afff282bfae10a2ee5f8e25ea9
felirx <felirx@gmail.com>
2025-01-02 10:55:19 +0200

    proper vi cursor support for ghostty & fish

---
+++ fish/conf.d/03_ghostty_vikeys.fish
@@ -0,0 +1,11 @@
+# Set cursor style based on vi mode
+function fish_vi_cursor --on-variable fish_bind_mode
+    switch $fish_bind_mode
+        case default
+            echo -en "\e[2 q" # block cursor
+        case insert
+            echo -en "\e[6 q" # line cursor
+        case visual
+            echo -en "\e[2 q" # block cursor
+    end
+end
